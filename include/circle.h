8a7d47501ffdaa2a663a502bfeb788eb824c83ec
Gustavo Araujo Carvalho <gustavo_carvalho@A307-32L.imd.local>
2019-02-26 08:44:22 -0300

    Finish circle algorithm

--- include/circle.h
+++ include/circle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <functional>
 #include "shape.h"
 #include "point.h"
 #include "pixel.h"
@@ -10,7 +11,8 @@ private:
 	Pixel m_color;
 
 	void draw(Canvas *canvas);
-	void draw2(Canvas *canvas);
+	void draw2(Canvas *canvas, const std::function<void (int,int)> &drawPoint);
+
 public:
 	Circle(const Point &_center, const int &_radius, const Pixel &_color);
 	Circle(const Point &_center, const int &_radius);
--- src/circle.cpp
+++ src/circle.cpp
@@ -9,34 +9,17 @@ Circle::Circle(const Point &_center, const int &_radius, const Pixel &_color)
 Circle::Circle(const Point &_center, const int &_radius) : Circle(_center, _radius, Pixel(0, 0, 0)) {}
 
 void Circle::draw(Canvas *canvas) {
-    int x = m_center.getX();
-    int y = m_center.getY() + m_radius;
-    int d = 1 - m_radius;
-
-    int deltaL = 3;
-    int deltaSE = -2 * m_radius + 5;
-
-    std::cout << "deltaL " << deltaL << ", deltaSE " << deltaSE << ", d " << d << "\n";
-    canvas->setPixel(Point(x, y), m_color);
-    while (y > x) {
-        if (d < 0) {
-            d += deltaL;
-            deltaL += 2;
-            deltaSE += 2;
-        } else {
-            d += deltaSE;
-            deltaL += 2;
-            deltaSE += 4;
-            y--;
-        }
-        x++;
-        std::cout << "deltaL " << deltaL << ", deltaSE " << deltaSE << ", d " << d << "\n";
-        canvas->setPixel(Point(x, y), m_color);
-    }
-    draw2(canvas);
+    draw2(canvas, [this, canvas](const int &x, const int &y) {canvas->setPixel(Point(m_center.getX() + x, m_center.getY() + y), m_color);});
+    draw2(canvas, [this, canvas](const int &x, const int &y) {canvas->setPixel(Point(m_center.getY() + y, m_center.getX() + x), m_color);});
+    draw2(canvas, [this, canvas](const int &x, const int &y) {canvas->setPixel(Point(m_center.getY() + y, m_center.getX() - x), m_color);});
+    draw2(canvas, [this, canvas](const int &x, const int &y) {canvas->setPixel(Point(m_center.getX() + x, m_center.getY() - y), m_color);});
+    draw2(canvas, [this, canvas](const int &x, const int &y) {canvas->setPixel(Point(m_center.getX() - x, m_center.getY() - y), m_color);});
+    draw2(canvas, [this, canvas](const int &x, const int &y) {canvas->setPixel(Point(m_center.getY() - y, m_center.getX() - x), m_color);});
+    draw2(canvas, [this, canvas](const int &x, const int &y) {canvas->setPixel(Point(m_center.getY() - y, m_center.getX() + x), m_color);});
+    draw2(canvas, [this, canvas](const int &x, const int &y) {canvas->setPixel(Point(m_center.getX() - x, m_center.getY() + y), m_color);});
 }
 
-void Circle::draw2(Canvas *canvas) {
+void Circle::draw2(Canvas *canvas, const std::function<void (int,int)> &drawPoint) {
     int x = 0;
     int y = m_radius;
     int d = 1 - m_radius;
@@ -44,8 +27,7 @@ void Circle::draw2(Canvas *canvas) {
     int deltaL = 3;
     int deltaSE = -2 * m_radius + 5;
 
-    std::cout << "deltaL " << deltaL << ", deltaSE " << deltaSE << ", d " << d << "\n";
-    canvas->setPixel(Point(m_center.getX() - x, m_center.getY() + y), m_color);
+    drawPoint(x, y);
     while (y > x) {
         if (d < 0) {
             d += deltaL;
@@ -58,7 +40,6 @@ void Circle::draw2(Canvas *canvas) {
             y--;
         }
         x++;
-        std::cout << "deltaL " << deltaL << ", deltaSE " << deltaSE << ", d " << d << "\n";
-        canvas->setPixel(Point(m_center.getX() - x, m_center.getY() + y), m_color);
+        drawPoint(x, y);
     }
 }
